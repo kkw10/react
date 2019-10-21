2ce45b842216420163d603aeaa19534af939a2ce
kkw_10 <kkw_10@naver.com>
2019-10-21 18:08:43 +0900

    외부 api를 이용하여 뉴스 뷰어 만들기

---
+++ news-viewer/src/components/NewsList.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled from 'styled-components';
+import NewsItem from './NewsItem';
+import axios from 'axios';
+import usePromise from '../lib/usePromise';
+
+const NewsListBlock = styled.div`
+  box-sizing: border-box;
+  padding-bottom: 3rem;
+  width: 768px;
+  margin: 0 auto;
+  margin-top: 2rem;
+
+  @media screen and (max-width: 768px) {
+    width: 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+  }
+`
+
+const sampleArticle = {
+  title: '제목',
+  description: '내용',
+  url: 'https://google.com',
+  urlToImage: 'https://via.placeholder.com/160'
+}
+
+const NewsList = ({ category }) => {
+  const [loading, response, error] = usePromise(() => {
+    const query = (category === 'all' ? '' : `&category=${category}`);
+    return axios.get(`https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=8a58857d57d3481688c6dbc361bd9c47`)
+  }, [category])
+
+  if (loading) {
+    return (
+      <NewsListBlock>대기 중...</NewsListBlock>
+    )
+  }
+
+  if (!response) {
+    return null;
+  }
+
+  const { articles } = response.data;
+
+  return (
+    <NewsListBlock>
+      {articles.map((article) => (
+        <NewsItem key={article.url} article={article} />
+      ))}
+    </NewsListBlock>
+  )
+};
+
+export default NewsList;
