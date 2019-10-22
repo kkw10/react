448213fd7c08afb9ae9653f9cafce09aa32d097f
kkw_10 <kkw_10@naver.com>
2019-10-22 14:47:47 +0900

    리액트 리덕스 튜토리얼

---
+++ react-redux-tutorial/src/containers/TodosContainer.js
@@ -0,0 +1,42 @@
+import React, { useCallback, memo } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeInput, insert, toggle, remove } from '../modules/todo';
+import Todos from '../components/Todos';
+
+const TodosContainer = () => {
+  const { input, todos } = useSelector(({ todos }) => ({
+    input: todos.input,
+    todos: todos.todos,
+  }))
+
+  const dispatch = useDispatch();
+
+  const onChangeInput = useCallback((input) => (
+    dispatch(changeInput(input))
+  ), [dispatch])
+
+  const onInsert = useCallback((text) => (
+    dispatch(insert(text))
+  ), [dispatch])
+
+  const onToggle = useCallback((id) => (
+    dispatch(toggle(id))
+  ), [dispatch])
+
+  const onRemove = useCallback((id) => (
+    dispatch(remove(id))
+  ), [dispatch])
+
+  return (
+    <Todos 
+      input={input}
+      todos={todos}
+      onChangeInput={onChangeInput}
+      onInsert={onInsert}
+      onToggle={onToggle}
+      onRemove={onRemove}
+    />
+  )
+}
+
+export default memo(TodosContainer);
